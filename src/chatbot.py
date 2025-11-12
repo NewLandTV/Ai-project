45b57f8439874fc2ef984c5896a2c35ae744d4a2
JkhTV <candzzang5283@gmail.com>
2025-11-12 21:11:10 +0900

    Feat(chatbot): load prompt
    Change chatbot model to exaone3.5. (korean)

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,9 +1,13 @@
 import ollama
+from prompt import get_prompt_info
 
 class Chatbot:
-    def __init__(self, model="mistral", messages=[{"role": "system", "content": "You are a helpful assistant."}]):
+    def __init__(self, model="mistral"):
         self.model = model
-        self.messages = messages
+        self.messages = []
+        prompt = get_prompt_info()
+        if prompt != None:  # prompt.txt가 존재하면 프롬프트 설정하기.
+            self.messages.append({"role": "system", "content": prompt})
 
     def get_response(self, message):
         self.messages.append({"role": "user", "content": message})
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import VsPyYt
 
 def main():
     print("인공지능 버튜버가 오고 있습니다...")
-    chatbot = Chatbot()
+    chatbot = Chatbot(model="exaone3.5")
     VsPyYt.start(chatbot)
 
 if __name__ == "__main__":
