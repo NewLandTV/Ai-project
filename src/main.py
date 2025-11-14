e6cd25d10e2f19762e9eb2b466adf9221d7c2947
JkhTV <candzzang5283@gmail.com>
2025-11-14 19:13:32 +0900

    Comment(main): write main code

--- src/main.py
+++ src/main.py
@@ -8,13 +8,19 @@ load_dotenv()
 
 def main():
     print("인공지능 버튜버가 오고 있습니다...")
+
+    # 챗봇 설정
     chatbot = Chatbot(model="exaone3.5")
+
+    # OBS 연동
     obs = OBS(
         os.getenv("obs_host"),
         os.getenv("obs_port"),
         os.getenv("obs_password")
     )
     obs.connect_obs_websocket()
+
+    # Vtube Studio, YouTube 실시간 채팅창 연동
     VsPyYt.start(chatbot, obs)
 
 if __name__ == "__main__":
