e62a2cd725541b5a57afcf9b71a39b25607d6ae7
JkhTV <candzzang5283@gmail.com>
2026-02-27 17:43:05 +0900

    Fix(command/ulibro): connection error

--- src/commands/ulibro/__init__.py
+++ src/commands/ulibro/__init__.py
@@ -7,7 +7,7 @@ def recv_message(sock: socket.socket):
             data = sock.recv(1024)
             print(f"API 서버에서 수신한 데이터 : {data.decode()}")
     except ConnectionError:
-        raise ConnectionError
+        print("API 서버와 연결이 종료되었습니다.")
     except:
         print("API 서버에서 데이터를 수신하는 과정에서 예외가 발생했습니다.")
 
@@ -25,8 +25,6 @@ def client_run():
             sock.send(msg.encode())
             if msg == "/exit":
                 break
-    except ConnectionError:
-        print("API 서버와 연결이 종료되었습니다.")
     except ConnectionAbortedError:
         print("API 서버에 연결할 수 없습니다.")
     except Exception as e:
@@ -39,5 +37,5 @@ def run():
     TODO: MyLibro와 연동하여 Ai project에서 MyLibro의 데이터를 접근 -> 도서 데이터를 처리함.
     Ai가 도서 데이터를 프롬프트로 명령을 처리하여 행동한 후의 결과를 대상에게 보냄.
     """
-    print("v0.1.0 ULibro 모듈이랍니다!")
+    print("v0.1.1 ULibro 모듈이랍니다!")
     client_run()
