a2b2fb8068f1a546e073b7c8efdf6a3f8a76f2ba
JkhTV <candzzang5283@gmail.com>
2026-02-27 17:32:39 +0900

    Feat(command): ulibro module connect mylibro api server
    Implemented Step II, but the AI is not processing the data yet.
    TODO: In this project, Ai understands the message entered by the users, accesses MyLibro data through the API server, and provides information from the book data.
    
    Related to: #9

--- src/commands/ulibro/__init__.py
+++ src/commands/ulibro/__init__.py
@@ -1,6 +1,43 @@
-"""
-TODO: MyLibro와 연동하여 Ai project에서 MyLibro의 데이터를 접근 -> 도서 데이터를 처리함.
-Ai가 도서 데이터를 프롬프트로 명령을 처리하여 행동한 후의 결과를 대상에게 보냄.
-"""
+import socket
+from threading import Thread
+
+def recv_message(sock: socket.socket):
+    try:
+        while True:
+            data = sock.recv(1024)
+            print(f"API 서버에서 수신한 데이터 : {data.decode()}")
+    except ConnectionError:
+        raise ConnectionError
+    except:
+        print("API 서버에서 데이터를 수신하는 과정에서 예외가 발생했습니다.")
+
+def client_run():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("127.0.0.1", 8230))
+
+        thread = Thread(target=recv_message, args=(sock,))
+        thread.daemon = True
+        thread.start()
+
+        while True:
+            msg = input("API 서버로 보낼 메시지 입력 ('/exit'로 종료): ")
+            sock.send(msg.encode())
+            if msg == "/exit":
+                break
+    except ConnectionError:
+        print("API 서버와 연결이 종료되었습니다.")
+    except ConnectionAbortedError:
+        print("API 서버에 연결할 수 없습니다.")
+    except Exception as e:
+        print(f"API 서버에 연결하는 도중에 예외가 발생했습니다. {e}")
+    finally:
+        sock.close()
+
 def run():
+    """
+    TODO: MyLibro와 연동하여 Ai project에서 MyLibro의 데이터를 접근 -> 도서 데이터를 처리함.
+    Ai가 도서 데이터를 프롬프트로 명령을 처리하여 행동한 후의 결과를 대상에게 보냄.
+    """
     print("v0.1.0 ULibro 모듈이랍니다!")
+    client_run()
